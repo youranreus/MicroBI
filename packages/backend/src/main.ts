714aa5979f373dac8b3d3d5a2e559051dc64e09a
季悠然 <youranreus@qq.com>
2024-02-28 20:56:38 +0800

    feat: backend base interceptor and filters

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  type NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { VERSION_NEUTRAL, VersioningType } from '@nestjs/common';
+
+import {
+  TransformInterceptor,
+  AllExceptionsFilter,
+  HttpExceptionFilter,
+} from '@reus-able/nestjs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.enableVersioning({
+    defaultVersion: [VERSION_NEUTRAL, '1'],
+    type: VersioningType.URI,
+  });
+
+  app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+
+  await app.listen(4000, '0.0.0.0');
 }
 bootstrap();
