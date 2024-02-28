f34da92bc1f1c6b56e9ae212f5c833d172eb1320
季悠然 <youranreus@qq.com>
2024-02-28 21:02:57 +0800

    feat: backend ValidationPipe & config & AuthGuard

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,10 +1,47 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_GUARD, APP_PIPE, Reflector } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  AuthGuard,
+  BusinessException,
+  HLOGGER_TOKEN,
+  HLogger,
+} from '@reus-able/nestjs';
+import { ENV_LIST } from './utils/constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [...ENV_LIST],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useFactory() {
+        return new ValidationPipe({
+          transform: true,
+          exceptionFactory: (errors) => {
+            const errorProperties = errors.map((e) => e.property).join(',');
+            return new BusinessException(
+              `参数校验失败，请检查 ${errorProperties}`,
+            );
+          },
+        });
+      },
+    },
+    {
+      provide: APP_GUARD,
+      useFactory(config: ConfigService, reflector: Reflector, logger: HLogger) {
+        return new AuthGuard(config, logger, reflector);
+      },
+      inject: [ConfigService, Reflector, HLOGGER_TOKEN],
+    },
+  ],
 })
 export class AppModule {}
---
+++ packages/backend/src/utils/constants.ts
@@ -0,0 +1,7 @@
+export const ENV_LIST = [
+  '.env.production.local',
+  '.env.development.local',
+  '.env.production',
+  '.env.development',
+  '.env',
+];
