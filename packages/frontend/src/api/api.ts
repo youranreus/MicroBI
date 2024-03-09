62e5aba8f8077f448f3dca3e54781573ba09ee10
季悠然 <youranreus@qq.com>
2024-03-09 12:17:35 +0800

    feat(frontend): 初始化api实例

---
+++ packages/frontend/src/api/api.ts
@@ -0,0 +1,20 @@
+import { createAlova } from 'alova'
+import VueHook from 'alova/vue'
+import { axiosRequestAdapter } from '@alova/adapter-axios'
+import { useUserStore } from '@/stores/user'
+import { ENV } from '@/utils'
+
+const alovaInstance = createAlova({
+  statesHook: VueHook,
+  requestAdapter: axiosRequestAdapter(),
+  baseURL: ENV.API,
+  beforeRequest(method) {
+    const { token } = useUserStore()
+
+    if (token.value) {
+      method.config.headers.Authorization = `Bearer ${token.value}`
+    }
+  }
+})
+
+export default alovaInstance
