1e11e4a0f02abb8041a461331e7e7088f8716a74
季悠然 <youranreus@qq.com>
2024-03-13 23:55:31 +0800

    feat(frontend): 数据源创建\&测试接口定义

--- packages/frontend/src/api/datasource.ts
+++ packages/frontend/src/api/datasource.ts
@@ -1,5 +1,16 @@
 import api from './api'
-import type { DatasourceListRes } from '@/types/datasource'
+import type {
+  DatasourceListRes,
+  DatasourceConnection,
+  DatasourceCreateParams
+} from '@/types/datasource'
+import type { Restful } from '@/types'
 
 export const getDatasourceList = (page = 1, size = 10, workspace?: number, search?: string) =>
   api.Get<DatasourceListRes>('/datasource', { params: { page, size, workspace, search } })
+
+export const testDatasourceConnection = (data: DatasourceConnection) =>
+  api.Put<Restful<{ status: boolean }>>('/datasource', data)
+
+export const createDatasource = (data: DatasourceCreateParams) =>
+  api.Post<Restful>('/datasource', data)
