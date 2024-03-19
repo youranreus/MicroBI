2d0566956888addb917ad3a7a92f61e8404d8be1
季悠然 <youranreus@qq.com>
2024-03-19 18:18:03 +0800

    feat(backend): chart dtos

--- packages/backend/src/dtos/chart/create-chart.dto.ts
+++ packages/backend/src/dtos/chart/create-chart.dto.ts
@@ -1,1 +1,30 @@
-export class CreateChartDto {}
+import { IsNotEmpty, ValidateNested, IsInt } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class ChartFieldDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsInt()
+  id: number;
+}
+
+export class CreateChartDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  type: string;
+
+  @ValidateNested()
+  @Type(() => ChartFieldDto)
+  quotas: ChartFieldDto[];
+
+  @ValidateNested()
+  @Type(() => ChartFieldDto)
+  dims: ChartFieldDto[];
+
+  @ValidateNested()
+  @Type(() => ChartFieldDto)
+  filters: ChartFieldDto[];
+}
