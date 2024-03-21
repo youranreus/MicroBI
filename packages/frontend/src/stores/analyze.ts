bd7bcc070e15aef56f88eaf32b03b89e3af14d6a
季悠然 <youranreus@qq.com>
2024-03-21 15:07:44 +0800

    feat(frontend): analyze store创建

---
+++ packages/frontend/src/stores/analyze.ts
@@ -0,0 +1,63 @@
+import type { DatasetMeta } from '@/types/dataset'
+import type { Field } from '@/types/field'
+
+const useStore = defineStore(
+  'analyze',
+  () => {
+    const quotas = ref<Field[]>([])
+    const dims = ref<Field[]>([])
+    const filters = ref<Field[]>([])
+    const dataset = ref<DatasetMeta>()
+    const allFields = ref<Field[]>([])
+
+    const changeDataset = (data: DatasetMeta) => {
+      dataset.value = data
+    }
+
+    const changeFields = (data: Field[]) => {
+      allFields.value = data
+    }
+
+    const updateField = (type: 'dim' | 'quota' | 'filter', data: Field[]) => {
+      const map: Record<'dim' | 'quota' | 'filter', Ref<Field[]>> = {
+        dim: dims,
+        quota: quotas,
+        filter: filters
+      }
+
+      map[type].value = data
+    }
+
+    const resetChart = () => {
+      quotas.value = []
+      dims.value = []
+      filters.value = []
+    }
+
+    return {
+      quotas,
+      dims,
+      filters,
+      dataset,
+      allFields,
+      changeDataset,
+      changeFields,
+      resetChart,
+      updateField
+    }
+  },
+  {
+    persist: {
+      key: 'analyze-store'
+    }
+  }
+)
+
+export const useAnalyzeStore = () => {
+  const store = useStore()
+
+  return {
+    ...store,
+    ...storeToRefs(store)
+  }
+}
