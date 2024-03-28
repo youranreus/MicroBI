78c5e388e847812bb3eb4f228dc0e2605abd7576
季悠然 <youranreus@qq.com>
2024-03-28 22:49:59 +0800

    feat(frontend): 数据字段展示弹窗

--- packages/frontend/src/views/analyze/components/field-item.vue
+++ packages/frontend/src/views/analyze/components/field-item.vue
@@ -1,11 +1,32 @@
 <template>
-  <div
-    :ref="drag"
-    class="py-2 px-3 rounded-lg bg-gray-100 flex cursor-grab select-none text-xs items-center gap-x-2"
-  >
-    <n-icon :component="FieldIconMap[field.type]"></n-icon>
-    <span class="text-ellipsis text-nowrap overflow-hidden">{{ field.name }}</span>
-  </div>
+  <n-popover placement="right-start">
+    <template #trigger>
+      <div
+        :ref="drag"
+        class="py-2 px-3 rounded-lg bg-gray-100 flex cursor-grab select-none text-xs items-center gap-x-2"
+      >
+        <n-icon :component="FieldIconMap[field.type]"></n-icon>
+        <span class="text-ellipsis text-nowrap overflow-hidden">{{ field.name }}</span>
+      </div>
+    </template>
+
+    <div class="w-[200px]">
+      <n-descriptions :columns="2" size="small">
+        <n-descriptions-item label="字段名">
+          {{ field.fieldname }}
+        </n-descriptions-item>
+        <n-descriptions-item label="展示名">
+          {{ field.name }}
+        </n-descriptions-item>
+        <n-descriptions-item label="ID">
+          {{ field.id }}
+        </n-descriptions-item>
+        <n-descriptions-item label="类型">
+          {{ field.type }}
+        </n-descriptions-item>
+      </n-descriptions>
+    </div>
+  </n-popover>
 </template>
 <script setup lang="ts">
 import type { Field } from '@/types/field'
