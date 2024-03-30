f774c41143c127d1884550a97e5b4e8bc81f81a9
季悠然 <youranreus@qq.com>
2024-03-30 18:06:55 +0800

    feat(frontend): analyze-title完善

--- packages/frontend/src/views/analyze/components/analyze-title.vue
+++ packages/frontend/src/views/analyze/components/analyze-title.vue
@@ -1,14 +1,41 @@
 <template>
-  <div class="text-lg font-bold flex items-center">
-    {{ name }}
+  <div v-if="!isEdit" class="flex items-center h-[34px]">
+    <div class="text-lg font-bold mr-1">{{ name }}</div>
+    <n-button quaternary size="small" circle @click="isEdit = true">
+      <template #icon>
+        <n-icon :component="Pencil"></n-icon>
+      </template>
+    </n-button>
   </div>
+  <n-input-group v-else>
+    <n-input
+      v-model:value="tmpValue"
+      autosize
+      style="min-width: 200px"
+      :placeholder="name"
+    ></n-input>
+    <n-button @click="handleEdit">完成</n-button>
+  </n-input-group>
 </template>
 <script setup lang="ts">
 import { useAnalyzeStore } from '@/stores/analyze'
+import { Pencil } from '@vicons/ionicons5'
 
 defineOptions({
   name: 'AnalyzeTitle'
 })
 
-const { name } = useAnalyzeStore()
+const { name, setName } = useAnalyzeStore()
+const isEdit = ref(false)
+const tmpValue = ref(name.value)
+
+const handleEdit = () => {
+  if (tmpValue.value) {
+    setName(tmpValue.value)
+  } else {
+    tmpValue.value = name.value
+  }
+
+  isEdit.value = false
+}
 </script>
