c8b592f951e38e284ff237428d968c413723b4c6
季悠然 <youranreus@qq.com>
2024-04-10 18:51:27 +0800

    feat(frontend): 看板删除图表

--- packages/frontend/src/views/dashboard/components/dashboard-grid-item.vue
+++ packages/frontend/src/views/dashboard/components/dashboard-grid-item.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="w-full h-full overflow-hidden border border-gray-200 p-3 rounded-lg bg-white">
-    <div class="header">
-      {{ chart.name }}
+    <div class="header flex justify-between items-center">
+      <n-h6 prefix="bar" align-text class="mb-0">
+        {{ chart.name }}
+      </n-h6>
+
+      <n-popconfirm positive-text="确定" negative-text="取消" @positive-click="handleDeleteChart">
+        <template #trigger>
+          <n-button v-if="editMode" size="small" circle secondary type="error">
+            <template #icon>
+              <n-icon :component="TrashOutline"></n-icon>
+            </template>
+          </n-button>
+        </template>
+        确定删除图表「{{ chart.name }}」吗
+      </n-popconfirm>
     </div>
   </div>
 </template>
 <script setup lang="ts">
+import { useDashboardStore } from '@/stores/dashboard'
 import type { DashboardChartItem } from '@/types/dashboard'
+import { TrashOutline } from '@vicons/ionicons5'
 
 defineOptions({
   name: 'DashboardGridItem'
@@ -17,4 +32,11 @@ const props = defineProps<{
 }>()
 
 const chart = computed(() => props.data.data)
+
+const { editMode, charts, updateCharts } = useDashboardStore()
+
+const handleDeleteChart = () => {
+  const newCharts = charts.value.filter((item) => item.data.id !== chart.value.id)
+  updateCharts(newCharts)
+}
 </script>
